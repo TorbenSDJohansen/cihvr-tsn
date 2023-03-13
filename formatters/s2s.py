54498e8854b102521112ae21c67f619d507e9194
Torben Skov Dyg Johansen <tsdj@sam.sdu.dk>
2023-03-13 11:45:12 +0100

    s2s style date formatter for CIHVR

--- formatters/s2s.py
+++ formatters/s2s.py
@@ -154,7 +154,72 @@ class CIHVRNamesFormatter(CharSeqSepSubsetFormatter):
         return clean
 
 
-# TODO date formatter (but NEED to change to add keep bad cpd option) from https://github.com/TorbenSDJohansen/timm-sequence-net/blob/main/timmsn/data/formatters/date_seq2seq.py
+class CIHVRDatesFormatter(CharSeqSepSubsetFormatter):
+    def __init__(self, handle_bad_cpd: str, bad_cpd_char: str, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        if not isinstance(handle_bad_cpd, str):
+            raise TypeError(f'handle_bad_cpd must be str, got {handle_bad_cpd}')
+
+        if not handle_bad_cpd in ('keep', 'drop'):
+            raise ValueError(f'handle_bad_cpd must be one of keep or drop, received {handle_bad_cpd}')
+
+        if not isinstance(bad_cpd_char, str):
+            raise TypeError(f'bad_cpd_char must be str, got {bad_cpd_char}')
+
+        if len(bad_cpd_char) != 1:
+            raise ValueError(f'bad_cpd_char must be 1 char, got {bad_cpd_char}')
+
+        self.handle_bad_cpd = handle_bad_cpd
+        self.bad_cpd_char = bad_cpd_char
+
+    def sanitize(self, raw_input: Union[None, str]) -> Union[None, str]:
+        if raw_input is None:
+            return None
+
+        if not isinstance(raw_input, str):
+            raise TypeError(
+                'Raw input must be string, received input ' +
+                f'{raw_input} of type {type(raw_input)}.'
+                )
+
+        if raw_input == 'bad cpd':
+            if self.handle_bad_cpd == 'drop':
+                return None
+
+            raw_input = self.bad_cpd_char
+
+        seq = self.cast_seps(raw_input).strip(self.sep_value)
+        seq = self.grab_subseq(seq)
+
+        for allowed_empty in self.allowed_empty:
+            seq = seq.replace(allowed_empty, '')
+
+        split = seq.split(self.sep_value)
+        mod_seq = []
+
+        for item in split: # remove leading 0s
+            mod_seq.append(item.lstrip('0'))
+
+        mod_seq = self.sep_value.join(mod_seq)
+
+        return mod_seq
+
+    def clean_pred(self, raw_pred: np.ndarray, assert_consistency: bool = True) -> Union[str, None]: # input: array of non-negative ints
+        clean = self.clean_seq(raw_pred, assert_consistency=assert_consistency)
+
+        if clean == self.bad_cpd_char: # FIXME what if just ANY part of clean is self.bad_cpd_char? Currently does not handle
+            clean = 'bad cpd'
+
+        split = clean.split(self.sep_value)
+        mod_clean = []
+
+        for item in split: # replace '' with proper ',' for missing
+            mod_clean.append(item if item else self.empty_value)
+
+        mod_clean = self.sep_value.join(mod_clean)
+
+        return mod_clean
 
 
 @register_formatter
@@ -238,6 +303,30 @@ def s2s_first_name_keep_bad_cpd() -> CIHVRNamesFormatter:
         map_idx_char=map_idx_char,
         allowed_empty={'', '0=Mangler'},
         empty_value='0=Mangler',
+        unk_value='?',
+        invalid_pred_value='InvalidPred',
+        )
+
+    return formatter
+
+
+@register_formatter
+def s2s_dates_keep_bad_cpd() -> CIHVRDatesFormatter:
+    bad_cpd_char = 'b'
+    map_char_idx, map_idx_char = construct_numseq_keep_bad_cpd_map(bad_cpd_char)
+
+    formatter = CIHVRDatesFormatter(
+        start=0,
+        end=2,
+        allowed_seps={':'},
+        sep_value=':',
+        handle_bad_cpd='keep',
+        bad_cpd_char=bad_cpd_char,
+        max_seq_len=5, # this is before BOS and EOS tokens added
+        map_char_idx=map_char_idx,
+        map_idx_char=map_idx_char,
+        allowed_empty={',', '0=Mangler'},
+        empty_value=',',
         unk_value='?',
         invalid_pred_value='InvalidPred',
         )
